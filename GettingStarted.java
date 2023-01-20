17ed8014a3e3a84090658399707246f02cbf80ce
Yug Patel <yugpathik7@gmail.com>
2023-01-19 22:14:30 -0500

    first commit

---
+++ GettingStarted.java
@@ -0,0 +1,16 @@
+/**
+ * GettingStarted
+ */
+public class GettingStarted {
+
+// this is why this matter
+
+    public static void main(String[] args) {
+        String one = "ABC123";
+        String two = "C";  
+        String three = "3";      
+        System.out.println(one.indexOf(two));     
+        System.out.println(one.indexOf(three));     
+        System.out.println(two.indexOf(one));
+    }
+}
