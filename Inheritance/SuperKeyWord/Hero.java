b4acf59323087d1855199bfabbb1a9c90fdfa409
Yug Patel <yugpathik7@gmail.com>
2023-02-14 19:17:16 -0500

    Super Key word Added

---
+++ Inheritance/SuperKeyWord/Hero.java
@@ -0,0 +1,10 @@
+package Inheritance.SuperKeyWord;
+
+public class Hero extends Person{
+    String power;
+
+    Hero(String name, int age, String power){
+        super(name, age);
+        this.power = power;
+    }
+}
---
+++ Inheritance/SuperKeyWord/Main.java
@@ -0,0 +1,13 @@
+package Inheritance.SuperKeyWord;
+
+public class Main {
+    public static void main(String[] args) {
+        Hero hero1 = new Hero("Batman", 42, "Rich");
+        
+
+
+        System.out.println(hero1.name);
+        System.out.println(hero1.age);
+        System.out.println(hero1.power);
+    }
+}
